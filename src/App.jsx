9b9dcb1410057dbf8b52dd7d0d9f9ad54400601b
prajapatiharshit2004 <prajapatiharshit2004@gmail.com>
2024-08-31 06:25:48 +0000

    systummm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+
 
 const App = () => {
   return (
-    <div className='text-xl font-italic'>App</div>
+    <div className='flex justify-center'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </div>
   )
 }
 
