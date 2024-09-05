5cb46507928efa6a7682e4fdda22b46255e1019d
Harshit <prajapatiharshit2004@gmail.com>
2024-09-05 13:11:11 +0530

    addedpaper1

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,10 +5,10 @@ import AnsButton from './AnsButton';
 const Body = (props) => {
     return (
         <>
-        <div className="absolute min-h-[100%] inset-0 -z-10 w-full items-center px-5 py-24 bg-black flex flex-col">
+        <div className="inset-0 -z-10  items-center px-5 py-24 bg-black flex flex-col">
             {props.paper.map((question, index) => {
-                return <div key={uuidv4} className='box-border border-3 border-blue-500 rounded-[22px] w-[95%] flex flex-col justify-center items-center my-[11px]'>
-                            <h2 className='text-white my-3 w-[95%] text-2xl font-poppins' >{`Q${index + 1}) ${question.ques}`}</h2>
+                return <div key={uuidv4} className='box-border bg-blue-900 border-2 border-green-500 rounded-[22px] w-[95%] flex flex-col justify-center items-center my-[11px] p-4 min-h-[322px]'>
+                            <h2 className='text-green-400 my-3 w-[95%] text-xl lg:text-2xl font-poppins' >{`Q${index + 1}) ${question.ques}`}</h2>
                             <div className='h-[212px] flex flex-col justify-around'>
                                 <AnsButton correct = {question.ans} option = "A" ansvalue = {question.A}/>
                                 <AnsButton correct = {question.ans} option = "B" ansvalue = {question.B}/>
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,31 +2,230 @@ export const data = [
     [
         {
             q_id: 1,
-            ques: "Who won wc 24? earn all about how to imlement font using the API CSS code. A web font is any font used in a websites design that isnt installed by default on the end users device—a counterpart to a system font. Check out the articles below for more guidance:",
-            A: "pak",
-            B: "ind",
-            C: "SA",
-            D: "aus",
-            ans: "B"
+            ques: "The length of the badminton court for sigles is :",
+            A: "13.44",
+            B: "13.55",
+            C: "14",
+            D: "13.40",
+            ans: "D"
         },
         {
             q_id: 2,
-            ques: "Who won wc 23?",
-            A: "pak",
-            B: "ind",
-            C: "SA",
-            D: "aus",
+            ques: "Who is the administrative head of Indian audit and Accounts Department?",
+            A: "Accountant General",
+            B: "Principal Accountant General",
+            C: "Director General",
+            D: "The controller and Auditor General",
             ans: "D"
         },
         {
             q_id: 3,
-            ques: "Who won wc 19?",
-            A: "pak",
-            B: "eng",
-            C: "SA",
-            D: "aus",
+            ques: "Which of the following is the largest pan-india scheme to strenghthen health care infrastructure across the country with focus on primary, secondary and tertiary care services ?",
+            A: "LaQshya",
+            B: "AP-PMJAY",
+            C: "PM-ABHIM",
+            D: "PM-MI",
+            ans: "C"
+        },
+        {
+            q_id: 4,
+            ques: "Which of the following festivals is associated with the term 'ties of protaection' ?",
+            A: "Baisakhi",
+            B: "Karwa Chauth",
+            C: "Chhath Pooja",
+            D: "Raksha Bandhan",
+            ans: "D"
+        },
+        {
+            q_id: 5,
+            ques: "In October 2021, 19-year old _____ won the silver medal at World Wrestling Championship.",
+            A: "Anshu Malik",
+            B: "Seema Bisla",
+            C: "Sanju Devi",
+            D: "Sonam Malik",
+            ans: "A"
+        },
+        {
+            q_id: 6,
+            ques: "_____ emerged as the poorest state as the first-ever Multi-dimensional Poverty index (MPI) prepared by Niti Aayog and launched in November 2021",
+            A: "Bihar",
+            B: "Madhya Pradesh",
+            C: "Uttar Pradesh",
+            D: "Mizoram",
+            ans: "A"
+        },
+        {
+            q_id: 7,
+            ques: "The consumption of fixed capital is also known as ______.",
+            A: "depreciation",
+            B: "net investment",
+            C: "appreciation",
+            D: "gross investment",
+            ans: "A"
+        },
+        {
+            q_id: 8,
+            ques: "The Vedic Aryans lived in the area called Sapt-Sindhu, which means area drained by seven rivers. One of the rivers among the seven in Jhelum. What was its ancient name ?",
+            A: "Askini",
+            B: "Parushni",
+            C: "Vipash",
+            D: "Vitasta",
+            ans: "D"
+        },
+        {
+            q_id: 9,
+            ques: "In Chola Administration, ____ was the assembly in the villages which were inhabited predominantly by the Brahmanas.",
+            A: "Ur",
+            B: "Khilya",
+            C: "Nagaram" ,
+            D: "Sabha",
+            ans: "D"
+        },
+        {
+            q_id: 10,
+            ques: "Which of the following is not a part of tapi Basin ?",
+            A: "Rajasthan",
+            B: "Maharastra",
+            C: "Madhya Pradesh",
+            D: "Gujarat",
+            ans: "A"
+        },
+        {
+            q_id: 11,
+            ques: "Which of the following is used as a cooling medium for the Large Hardon Collider (LHC) and the superconducting magnets in MRI scanners and NMR spectometers ?",
+            A: "Neon",
+            B: "Chlorine",
+            C: "Argon",
+            D: "Helium",
+            ans: "D"
+        },
+        {
+            q_id: 12,
+            ques: "In 2018, Google Doodle celebrated the 100th Birthday of Mrilanini Sarabhai. She is an exponent of which of the following dance forms ?",
+            A: "Bharatanatyam and Kathakali",
+            B: "Odissi and Kathak",
+            C: "Kuchipudi and Bharathanatyam",
+            D: "Yakshagana",
+            ans: "A"
+        },
+        {
+            q_id: 13,
+            ques: "Bharatanayam ecpresses South indian religious thhemes and spritual ideas of ______",
+            A: "Sufism",
+            B: "Shaivism",
+            C: "Buddhism",
+            D: "Jainism",
             ans: "B"
         },
+        {
+            q_id: 14,
+            ques: "Who among the following musicians is popular for his mastery over the musical intrument Sitar ?",
+            A: "Ali Akbar Khan",
+            B: "Vilayat khan",
+            C: "Bahadur Khan",
+            D: "Amjad Ali Khan",
+            ans: "B"
+        },
+        {
+            q_id: 15,
+            ques: "Which of the following countries was the host of AFC Women's asia Cup Football-2022 ?",
+            A: "Japan",
+            B: "Bangladesh",
+            C: "India",
+            D: "China",
+            ans: "C"
+        },
+        {
+            q_id: 16,
+            ques: "Ms Bhakti Pradip Kulkarni was conferrd with the Arjuna Award 2022 for her outstanding contribution in which of the following sports ?",
+            A: "Table Tennis",
+            B: "Chess",
+            C: "Badminton",
+            D: "Wrestling",
+            ans: "B"
+        },
+        {
+            q_id: 17,
+            ques: "Who among the following was the founder of 'Tiger Legion' of 'Free India Legion' ?",
+            A: "Vinayak Damodar Savakar",
+            B: "Subhas Chandra Bose",
+            C: "Sohan Singh Bhakhna",
+            D: "Lala Hardayal",
+            ans: "B"
+        },
+        {
+            q_id: 18,
+            ques: "Name a reproductive strategy in which parasites take advantage of the care of other individuals of the same species or different species to raise their young.",
+            A: "Brood parasitism",
+            B: "Sexual parasitism",
+            C: "Klepto parasitism",
+            D: "Competitive parasitism",
+            ans: "A"
+        },
+        {
+            q_id: 19,
+            ques: "On the basis of tribal population (2011), identify the option that arranges the following states in ascending order. \nA. Madhya Pradesh. \nB. Maharastra \nC. Odisha ",
+            A: "(C)(B)(A)",
+            B: "(B)(A)(C)",
+            C: "(C)(A)(B)",
+            D: "(B)(C)(A)",
+            ans: "A"
+        },
+        {
+            q_id: 20,
+            ques: "In which industrial policy was the investment limit for tiny industry/unit increased to 2 Lakhs ?",
+            A: "1977",
+            B: "1991",
+            C: "1980",
+            D: "1956",
+            ans: "C"
+        },
+        {
+            q_id: 21,
+            ques: "Which scientist thought of the concept of steady state of the universe ?",
+            A: "Harold Jeffrey",
+            B: "Edwin Hubble",
+            C: "Fred Hoyle",
+            D: "Pierre-Simon Laplace",
+            ans: "C"
+        },
+        {
+            q_id: 22,
+            ques: "Match the columns. i) Chlorophyceae a) Brown algae \n ii) Phaeophyceae b) Green algae \n iii) Rhodophyceae  c)Blue-green algae \n iv) Cyanophyceae d) red algae",
+            A: " i-b ,ii-c, iii-a ,iv-d",
+            B: " i-b ,ii-a, iii-d, iv-c",
+            C: " i-d ,ii-c, iii-b ,iv-a",
+            D: " i-a ,ii-b, iii-c ,iv-d",
+            ans: "B"
+        },
+        {
+            q_id: 23,
+            ques: "Ryotwari system of revenue collection in india, introduced by the British, was based on the _____ .",
+            A: "Smith's theory of rent.",
+            B: "Ricardian theory of rent.",
+            C: "Malthusian theory of rent.",
+            D: "Marx's theory of rent.",
+            ans: "B"
+        },
+        {
+            q_id: 24,
+            ques: "Who received the Nobel Proze in 1901 for 'recognition of thte extraordinary services rendered by the discovery of the laws of chemical dynamics and osmotic pressure in solutions' ?",
+            A: "Hermann Emil Fisher",
+            B: "Jacobus Henricus van't Hoff",
+            C: "Svante August Arrhenius",
+            D: "Henri Moissan",
+            ans: "B"
+        },
+        {
+            q_id: 25,
+            ques: "Which of the following Aricles of the Indian Constitution are related to citizenship ?",
+            A: "Articles 15 to 21",
+            B: "Articles 5 to 11",
+            C: "Articles 2 to 4",
+            D: "Articles 25 to 31",
+            ans: "B"
+        },
+        
     ],
     [
         {
